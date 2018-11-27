5c497b44745bbfd03b209daeccdcbb840cc6fd77
wuSweet <2845075190@qq.com>
2018-11-27 08:54:32 +0800

    项目初始结构

---
+++ src/main/java/com/example/demo/Springbooot1127Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooot1127Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooot1127Application.class, args);
+	}
+}
